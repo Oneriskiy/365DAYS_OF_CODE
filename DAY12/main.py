dccee655b880f830254a06908be93105b5daf6ca
Герман <german@MacBook-Air-German.local>
2026-03-22 00:10:41 +0300

    Added get, post, and pydantic

--- DAY12/main.py
+++ DAY12/main.py
@@ -2,28 +2,75 @@ from fastapi import FastAPI, HTTPException
 import uvicorn
 from pydantic import BaseModel
 
+
+class Name(BaseModel):
+    name: str
+
+
 class Monument(BaseModel):
     title: str
     price: int
 
+
 app = FastAPI()
 
 monuments = {
-        "granit": 15000,
-        "mramore": 35000
+        "granite": 15000,
+        "marble": 35000,
+        'limestone': 54000
      }
 
 
-@app.get("/monuments")
+name = {
+    "name": "guest"
+    }
+
+
+@app.get("/",
+         summary="Username here",
+         description="Returns the username",
+         tags=['main', 'main_get']
+         )
+def main_get():
+    if not name:
+        raise HTTPException(status_code=404, detail='Not Found')
+    return name
+
+
+
+@app.post("/",
+            summary="Username here",
+            description="Gets the username",
+            tags=['main', 'main_post']
+)
+def main_post(user: Name):
+    name["name"] = user.name
+    return {'message': f"Welcome! {name['name']}"}
+
+
+@app.get("/monuments",
+         summary='monuments here',
+         description='gets all monuments',
+         tags=['monuments', 'monuments_get'])
 def func_monument():
+    if not monuments:
+        raise HTTPException(status_code=404, detail='Not found')
     return monuments
 
-@app.post("/monuments")
+
+@app.post("/monuments/m",
+            summary="monuments here",
+            description = "receives all the monuments",
+            tags=['monuments', 'monuments_post']
+          )
 def add_monument(monument: Monument):
     monuments[monument.title] = monument.price
-    return {"message": f"Монумент '{monument.title}' добавлен", "monuments": monuments}
+    return {"message": f"Monument '{monument.title}' added", "monuments": monuments}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port = 5000, reload=True)
+    uvicorn.run("main:app",
+                host = 'localhost',
+                port = 5000,
+                reload=True)
 

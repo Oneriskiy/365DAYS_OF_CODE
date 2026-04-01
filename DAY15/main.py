8cf521aed10573f140bc762dc07d0418518126f6
Герман <german@MacBook-Air-German.local>
2026-04-01 04:01:37 +0300

    I made a mini-game guess the word  with generators

--- DAY15/main.py
+++ DAY15/main.py
@@ -1,45 +1,57 @@
 import random
+from tqdm import tqdm
 
-texts_fruits = {1: 'apple',
-                2: 'orange',
-                3: 'pineapple',
-                4: 'qiwi',
-                5: 'banana',
-                6: 'melon'
-}
+class Colors:
+        red = '\033[31m'
+        green = '\033[32m'
+        purple = '\033[35m'
+        reset = '\033[0m'
 
-texts_animals = {
-    7: 'wolf',
-    8: 'rabbit',
-    9: 'parrot',
-    10: "chicken",
-    11: 'sheep',
-    12: 'crocodile'
-}
-
-all_texts = texts_fruits | texts_animals
 
 def generator(texts):
-    for text, text_two in texts.items():
-        if len(text_two) > 3:
-            yield text_two
-        else:
-            continue
+    for id, text in texts.items():
+        if len(text) > 3:
+            yield text
 
 
-def gen_output(gen):
+def gen_output(gen, total):
+    bar = tqdm(total=total)
     while True:
         try:
-            gens = next(gen)
-            print(gens.replace(random.choice(gens),'-'))
-            asc = input("enter answer:")
-            if asc == gens: print("true")
-            else: print("false")
+            my_gen = next(gen)
+            print(next(gen_id),my_gen.replace(random.choice(my_gen),'-'))
+            asc = input(f"{Colors.purple}enter answer:{Colors.reset}")
+            if asc == my_gen:
+                print(f"{Colors.green}true{Colors.reset}")
+                bar.update(1)
+            else:
+                print(f"{Colors.red}false{Colors.reset}")
+                bar.update(1)
         except StopIteration:
             print("the words are over")
+            bar.close()
             break
 
 
 if __name__ == "__main__":
+    texts_fruits = {1: 'apple',
+                    2: 'orange',
+                    3: 'pineapple',
+                    4: 'qiwi',
+                    5: 'banana',
+                    6: 'melon'
+                    }
+
+    texts_animals = {
+        7: 'wolf',
+        8: 'rabbit',
+        9: 'parrot',
+        10: "chicken",
+        11: 'sheep',
+        12: 'crocodile'
+    }
+
+    all_texts = texts_fruits | texts_animals
+    gen_id = (i+1 for i in range(0, 10000000))
     gen = generator(all_texts)
-    gen_output(gen)
+    gen_output(gen, total=len(all_texts))
